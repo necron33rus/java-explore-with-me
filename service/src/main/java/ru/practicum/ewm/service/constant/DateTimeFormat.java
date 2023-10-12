a4ffd0971c92dad32fb440ddcf6fdbe48d0653f3
necron33rus <egor.koshchenkov@yandex.ru>
2023-10-12 18:40:20 +0300

    fix: checkstyle

--- service/src/main/java/ru/practicum/ewm/service/constant/DateTimeFormat.java
+++ service/src/main/java/ru/practicum/ewm/service/constant/DateTimeFormat.java
@@ -2,7 +2,9 @@ package ru.practicum.ewm.service.constant;
 
 public class DateTimeFormat {
 
-    public DateTimeFormat() {}
+    public DateTimeFormat() {
+
+    }
 
     public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 }
