54d6c92aa91d76bf0fad9ed1d5a7910f5ff80dd4
necron33rus <egor.koshchenkov@yandex.ru>
2023-10-12 21:38:16 +0300

    fix: postman tests

--- service/src/main/java/ru/practicum/ewm/service/exception/CustomExceptionHandler.java
+++ service/src/main/java/ru/practicum/ewm/service/exception/CustomExceptionHandler.java
@@ -3,18 +3,19 @@ package ru.practicum.ewm.service.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
+@RestControllerAdvice("ru.practicum.ewm")
 public class CustomExceptionHandler {
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleBadRequestException(final MissingServletRequestParameterException e) {
+    public ApiError handleBadRequestException(final BadRequestException e) {
         return ApiError.builder()
                 .status(HttpStatus.BAD_REQUEST)
                 .reason("Incorrectly made request.")
@@ -23,7 +24,7 @@ public class CustomExceptionHandler {
                 .build();
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleBindException(final BindException e) {
         return ApiError.builder()
@@ -47,9 +48,9 @@ public class CustomExceptionHandler {
                 .build();
     }
 
-    @ExceptionHandler({NotFoundException.class})
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleNotFoundException(final RuntimeException e) {
+    public ApiError handleNotFoundException(final NotFoundException e) {
         return ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .reason("The required object was not found.")
@@ -58,9 +59,9 @@ public class CustomExceptionHandler {
                 .build();
     }
 
-    @ExceptionHandler({ConflictException.class})
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleConflictException(final RuntimeException e) {
+    public ApiError handleConflictException(final ConflictException e) {
         return ApiError.builder()
                 .status(HttpStatus.FORBIDDEN)
                 .reason("For the requested operation the conditions are not met.")
@@ -69,7 +70,7 @@ public class CustomExceptionHandler {
                 .build();
     }
 
-    @ExceptionHandler({DataIntegrityViolationException.class})
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleDataIntegrityViolationException(final DataIntegrityViolationException e) {
         return ApiError.builder()
@@ -82,7 +83,7 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiError handleUnhandled(final Throwable e) {
+    public ApiError handleUnhandled(final Exception e) {
         return ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .reason("Internal server error.")
